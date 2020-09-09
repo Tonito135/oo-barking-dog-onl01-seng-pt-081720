cb572c6ac49fb246bc0d7e5180e1b3f922d204e9
Anthony Hernandez <anthonyh2020@gmail.com>
2020-09-09 21:22:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(dogs_name)
+    @the_dogs_name = dogs_name
+  end
+  def name
+    @the_dogs_name
+  end
+  fido = Dog.new
+  fido.name = "fido"
+  name.bark
+  puts "woof!"
+end
+
